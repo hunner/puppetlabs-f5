8be946d5d4d2d565c91fc8895599f33a81c5e982
Ashley Penney <ashley.penney@puppetlabs.com>
2014-03-18 17:16:40 -0400

    Port this over to savon.
    Lots of changes here to make this work with the returned savon hash
    structure.  We need to continue to clean this up.

--- lib/puppet/util/network_device/f5/facts.rb
+++ lib/puppet/util/network_device/f5/facts.rb
@@ -28,29 +28,25 @@ class Puppet::Util::NetworkDevice::F5::Facts
         @facts[key] = @transport[F5_WSDL].send("get_#{key}".to_s)
     end
 
-    # Not sure if there's a cleaner way to get SOAP Mapping Object attributes.
-    # maybe if we use Savon to get a hash back instead of this kludge.
-    soap = SOAP::Mapping::Object.new
-
-    system_info = @transport[F5_WSDL].get_system_information
-    attributes  = system_info.methods.reject{|k| k =~ /=$/} - soap.methods
-    attributes.each { |key| @facts[key] = system_info[key] }
-
-    hardware_info = @transport[F5_WSDL].get_hardware_information
-    attributes    = hardware_info.first.methods.reject{|k| k =~ /=$/} - soap.methods
-    attributes.each { |key| @facts["hardware_#{key}"] = hardware_info.first[key] }
-    hardware_info.each do |hardware|
-      attributes = hardware.methods.reject{|k| k =~ /=$/} - soap.methods
-      attributes.each do |key|
-        fact_key = key == 'name' ? "hardware_#{hardware.name}" : "hardware_#{hardware.name}_#{key}"
-        @facts[fact_key] = hardware[key]
-      end
+    # TODO:  Make this an array of items to get, so we can determine the _response
+    # to use.
+    system_info = @transport[F5_WSDL].call(:get_system_information).to_hash
+    system_info[:get_system_information_response][:return].each do |key|
+      @facts[key] = system_info[:get_system_information_response][:return][key]
     end
 
-    disk_info = @transport[F5_WSDL].get_disk_usage_information
-    disk_info.usages.each do |disk|
-      @facts["disk_size_#{disk.partition_name.gsub('/','')}"] = "#{(to_64i(disk.total_blocks) * to_64i(disk.block_size))/1024/1024} MB"
-      @facts["disk_free_#{disk.partition_name.gsub('/', '')}"] = "#{(to_64i(disk.free_blocks) * to_64i(disk.block_size))/1024/1024} MB"
+    # We only want entries from the first item.
+    hardware_info = @transport[F5_WSDL].call(:get_hardware_information).to_hash
+    hardware_info[:get_hardware_information_response][:return].each do |key|
+      @facts["hardware_#{key}"] = hardware_info[:get_hardware_information_response][:return].first[key]
+      fact_key = key == 'name' ? "hardware_#{hardware.name}" : "hardware_#{hardware.name}_#{key}"
+      @facts[fact_key] = hardware[:get_hardware_information_response][:return][key]
+    end
+
+    disk_info = @transport[F5_WSDL].call(:get_disk_usage_information).to_hash
+    disk_info[:get_disk_usage_information_response][:return][:usages][:item].each do |disk|
+      @facts["disk_size_#{disk[:partition_name].gsub('/','')}"]  = "#{(to_64i(disk[:total_blocks]) * to_64i(disk[:block_size]))/1024/1024} MB"
+      @facts["disk_free_#{disk[:partition_name].gsub('/', '')}"] = "#{(to_64i(disk[:free_blocks]) * to_64i(disk[:block_size]))/1024/1024} MB"
     end
 
     # cleanup of f5 output to match existing facter key values.
@@ -66,18 +62,21 @@ class Puppet::Util::NetworkDevice::F5::Facts
       @facts['hostname'] = fqdn.shift
       @facts['domain']   = fqdn
     end
+
     if @facts['uptime_seconds'] then
       @facts['uptime']       = "#{String(@facts['uptime_seconds']/86400)} days" # String
       @facts['uptime_hours'] = @facts['uptime_seconds'] / (60 * 60)             # Integer
       @facts['uptime_days']  = @facts['uptime_hours'] / 24                      # Integer
     end
+
     if @facts['hardware_cpus_versions']
       @facts['hardware_cpus_versions'].each { |key| @facts["hardware_#{key.name.downcase.gsub(/\s/,'_')}"] = key.value }
       @facts.delete('hardware_cpus_versions')
       @facts.delete('hardware_information')
       @facts.delete('versions')
     end
-    @facts['timezone'] = @transport[F5_WSDL].get_time_zone.time_zone
+
+    @facts['timezone'] = @transport[F5_WSDL].call(:get_time_zone)[:get_time_zone_response][:return][:time_zone]
     @facts
   end
 end
