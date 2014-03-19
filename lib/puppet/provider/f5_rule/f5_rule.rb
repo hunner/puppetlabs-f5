54b8d98c1ffae4f09530732ab02f2d0149210f64
Ashley Penney <ashley.penney@puppetlabs.com>
2014-03-19 15:40:51 -0400

    Port this over to Savon.

--- lib/puppet/provider/f5_rule/f5_rule.rb
+++ lib/puppet/provider/f5_rule/f5_rule.rb
@@ -15,34 +15,37 @@ Puppet::Type.type(:f5_rule).provide(:f5_rule, :parent => Puppet::Provider::F5) d
   end
 
   def self.instances
-    transport[wsdl].get_list.collect do |name|
-      new(:name => name)
+    transport[wsdl].call(:get_list).body[:get_list_response][:return][:item].collect do |name|
+      new(:name   => name,
+          :ensure => :present
+         )
     end
   end
 
   def definition
     Puppet.debug("Puppet::Provider::F5_Rule: retrieving #{resource[:name]} rule definition")
-    transport[wsdl].query_rule(resource[:name]).first.rule_definition
+    output = transport[wsdl].call(:query_rule, message: { 'rule_names' => resource[:name] })
+    output.body[:query_rule_response][:return]
   end
 
   def definition=(val)
     Puppet.debug("Puppet::Provider::F5_Rule: updating #{resource[:name]} rule definition")
     rule = {"rule_name" => resource[:name], "rule_definition" => resource[:definition]}
-    transport[wsdl].modify_rule([rule])
+    transport[wsdl].call(:modify_rule, message: rule)
   end
 
   def create
     Puppet.debug("Puppet::Provider::F5_Rule: creating #{resource[:name]}")
     rule = {"rule_name" => resource[:name], "rule_definition" => resource[:definition]}
-    transport[wsdl].create([rule])
+    transport[wsdl].call(:create, message: rule)
   end
 
   def destroy
     Puppet.debug("Puppet::Provider::F5_Rule: destroying #{resource[:name]}")
-    transport[wsdl].delete_rule(resource[:name])
+    transport[wsdl].call(:delete_rule, message: resource[:name])
   end
 
   def exists?
-    transport[wsdl].get_list.include?(resource[:name])
+    transport[wsdl].call(:get_list).body.include?(resource[:name])
   end
 end
